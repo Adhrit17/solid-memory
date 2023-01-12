2153a2753fdface1ff0e110f5d76c6eccb9c7e13
Adhrit17 <108771106+Adhrit17@users.noreply.github.com>
2023-01-11 20:58:11 -0600

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+function addUser()
+{
+player_1_name = document.getElementById("player_1_name_input").value;   
+player_2_name = document.getElementById("player_2_name_input").value;
+
+localStorage.setItem("player 1",player_1_name);
+localStorage.setItem("player 2",player_2_name);
+window.location = "game_page.html";
+}
